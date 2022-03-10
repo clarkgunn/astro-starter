ec999d89f3d0d12a7b1b4cc96134833c5cd86d8a
ClarkMitchell <clarkmitchelldev@gmail.com>
2022-03-10 09:25:14 -0500

    Reduce dependence on sass.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    sitemap: false, // Generate sitemap (set to "false" to disable)
+    sitemap: false,
   },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-    // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-  },
+  devOptions: {},
   renderers: [],
 };
